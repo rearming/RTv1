15a865045bd04d2fc154cea030437677f585377d
sleonard <sleonard@student.42.fr>
2019-12-26 19:16:42 +0300

    compile.sh with updating libft submodule

--- compile.sh
+++ compile.sh
@@ -1,3 +1,10 @@
+if [[ ! -d "libft" ]]; then
+  git submodule init
+  git submodule update --merge --remote
+fi
+cd libft || echo "can't find libft directory!"
+make
+cd ..
 mkdir build
 cd build || echo "can't make build directory!"
 cmake -DCMAKE_BUILD_TYPE=Release ..
